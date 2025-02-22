ad4d91a417501b66cd5eb1be7d72fea53d588b30
turzo15-5785 <turzo15-5785@diu.edu.bd>
2025-02-22 04:39:15 +0000

    Cahced_Network_Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,17 +16,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Lab06 Advance UI'),
         ),
-        body: Column( // Use Column to stack images vertically
+        body: Column(
           children: [
-            Center( // First image centered
-              child: Image.network(
-                'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI',
-              ),
+            CachedNetworkImage(
+              imageUrl: 'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI',
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
-            Expanded( // Expanded to take remaining space
+            Expanded(
               child: Image.network(
                 'https://picsum.photos/id/870/200/300?grayscale&blur=2',
-                fit: BoxFit.cover, // Ensures the image fills the space properly
+                fit: BoxFit.cover,
               ),
             ),
           ],
