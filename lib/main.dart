19652e518a41d2abbdc52ceb5606408ed22d83c8
turzo15-5785 <turzo15-5785@diu.edu.bd>
2025-02-22 06:24:03 +0000

    Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,20 +12,62 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lab06 Advanced UI',
       home: Scaffold(
+        drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Row(
+                  children: [
+                    Icon(Icons.people_alt_rounded),
+                    Text('Contacts', style: TextStyle(color: Colors.black)),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.mobile_friendly),
+                title: Text("Device"),
+                trailing: Text('0'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Settings"),
+                trailing: Text('1'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("Info"),
+                trailing: Text('2'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: const Text('Lab06 Advance UI'),
+          backgroundColor: Colors.amber,
         ),
-        body: Column(
-          children: [
-            Align(
-              alignment: Alignment.topCenter, // Aligns image to the top center
-              child: CachedNetworkImage(
-                imageUrl:
-                    'https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI',
-                placeholder: (context, url) =>
-                    const CircularProgressIndicator(),
-                errorWidget: (context, url, error) => const Icon(Icons.error),
-              ),
+        body: Center(
+          child: Text(
+            "Working with Navigation",
+            style: TextStyle(fontSize: 60),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: "Search",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
             ),
           ],
         ),
